8edb0cc4d611035418dbfc38756ff80c649811a6
cristina murga <crismurga18@gmail.com>
2023-11-22 12:59:25 +0100

    creazione controller

---
+++ Controllers/ProductModelsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pedalacom.Models;
+
+namespace Pedalacom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductModelsController : ControllerBase
+    {
+        private readonly AdventureWorksLt2019Context _context;
+
+        public ProductModelsController(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductModels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetProductModels()
+        {
+          if (_context.ProductModels == null)
+          {
+              return NotFound();
+          }
+            return await _context.ProductModels
+                .Include(mod=> mod.Products)
+                .Include(mod => mod.ProductModelProductDescriptions)
+                .ToListAsync();
+        }
+
+        // GET: api/ProductModels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductModel>> GetProductModel(int id)
+        {
+          if (_context.ProductModels == null)
+          {
+              return NotFound();
+          }
+            var productModel = await _context.ProductModels
+                .Include(mod => mod.Products)
+                .Include(mod => mod.ProductModelProductDescriptions)
+                .FirstOrDefaultAsync(mod => mod.ProductModelId == id);
+
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return productModel;
+        }
+
+        // PUT: api/ProductModels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductModel(int id, ProductModel productModel)
+        {
+            if (id != productModel.ProductModelId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductModels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductModel>> PostProductModel(ProductModel productModel)
+        {
+          if (_context.ProductModels == null)
+          {
+              return Problem("Entity set 'AdventureWorksLt2019Context.ProductModels'  is null.");
+          }
+            _context.ProductModels.Add(productModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductModel", new { id = productModel.ProductModelId }, productModel);
+        }
+
+        // DELETE: api/ProductModels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductModel(int id)
+        {
+            if (_context.ProductModels == null)
+            {
+                return NotFound();
+            }
+            var productModel = await _context.ProductModels.FindAsync(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductModels.Remove(productModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductModelExists(int id)
+        {
+            return (_context.ProductModels?.Any(e => e.ProductModelId == id)).GetValueOrDefault();
+        }
+    }
+}
