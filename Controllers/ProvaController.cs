6b1dfeca8fb397eec9290a951784fdedf9d92d16
OssamaElBachraoui <ossama20915@gmail.com>
2023-11-28 18:00:43 +0100

    Creazione prova per view

--- Controllers/ProvaController.cs
+++ Controllers/ProvaController.cs
@@ -5,50 +5,36 @@ using Pedalacom.Models;
 namespace Pedalacom.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class ProvaController : ControllerBase
+[Route("[controller]")]
+public class ProvaController : ControllerBase
+{
+    private readonly AdventureWorksLt2019Context _context;
+
+    public ProvaController(AdventureWorksLt2019Context context)
     {
-        private readonly AdventureWorksLt2019Context _context;
-
-        public ProvaController(AdventureWorksLt2019Context context)
-        {
-            _context = context;
-        }
-
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductProva>>> getProductProva()
-        {
-            if(_context == null)
-            {
-                return NotFound();
-            }
-            return await _context.Products
-          .FromSqlRaw($"select p.ProductID, p.Name, p.Color, p.ListPrice, p.Size, p.Weight, pc.ProductCategoryID, pc.ParentProductCategoryID, pc.Name, pm.Name, pd.Description\r\nfrom SalesLT.Product p join SalesLT.ProductCategory pc on p.ProductCategoryID=pc.ProductCategoryID\r\njoin SalesLT.ProductModel pm on p.ProductModelID=pm.ProductModelID \r\njoin SalesLT.ProductModelProductDescription pmpd on p.ProductModelID=pmpd.ProductModelID\r\njoin SalesLT.ProductDescription pd on pmpd.ProductDescriptionID=pd.ProductDescriptionID")
-          .OrderBy(ob => ob.ProductId)
-          .ToListAsync();
-
-            // Passare i dati al
-        }
-
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
-        //{
-        //    if (_context.Customers == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return await _context.Customers
-        //        .Include(emp => emp.CustomerAddresses)
-        //        .Include(emp => emp.SalesOrderHeaders)
-        //        .ToListAsync();
-        //}
-
-    //        var page = await _context.ReducedCustomers.FromSql(
-    //$"SELECT CustomerId,FirstName,LastName,CompanyName,EmailAddress FROM [SalesLT].[Customer]")
-    //.OrderBy(ob => ob.CustomerId)
-    //.Where(c => c.CustomerId > dataPosition)
-    //.ToListAsync();
-    //    var x = DbSocieta.Database.SqlQuery<Impiegato>($"sp_scaffolding {id}");
+        _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductProva>>> GetProductProva()
+    {
+        if (_context == null)
+        {
+            return NotFound();
+        }
+
+        var products = await _context.Products
+                .FromSqlRaw("SELECT * FROM ViewProdotti")
+            .OrderBy(ob => ob.ProductId)
+            .ToListAsync();
+
+        if (products == null || products.Count == 0)
+        {
+            return NoContent(); 
+        }
+
+        return Ok(products); 
+    }
+}
+
 }
--- Models/ProductProva.cs
+++ Models/ProductProva.cs
@@ -2,16 +2,25 @@
 {
     public class ProductProva
     {
-        public int Id;
-        public string Name;
-        public string Color;
-        public double Price;
-        public string Size;
-        public double Weight;
-        public int CategoryId;
-        public int ParentCategoryId;
-        public string Category;
-        public string Model;
-        public string Description;
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public decimal ListPrice { get; set; }
+        public string Size { get; set; }
+        public double Weight { get; set; }
+        public int ProductCategoryID { get; set; }
+        public int ProductModelID { get; set; }
+        public int Expr1 { get; set; }
+        public int ParentProductCategoryID { get; set; }
+        public string Expr2 { get; set; }
+        public int Expr3 { get; set; }
+        public string Expr4 { get; set; }
+        public int Expr5 { get; set; }
+        public int ProductDescriptionID { get; set; }
+        public int Expr6 { get; set; }
+        public string Description { get; set; }
     }
+
+
+
 }
