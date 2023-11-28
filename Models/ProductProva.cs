442812081848f15b14879bd9f5319309252e004b
cristina murga <crismurga18@gmail.com>
2023-11-28 17:33:47 +0100

    query prodotto

---
+++ Models/ProductProva.cs
@@ -0,0 +1,17 @@
+﻿namespace Pedalacom.Models
+{
+    public class ProductProva
+    {
+        public int Id;
+        public string Name;
+        public string Color;
+        public double Price;
+        public string Size;
+        public double Weight;
+        public int CategoryId;
+        public int ParentCategoryId;
+        public string Category;
+        public string Model;
+        public string Description;
+    }
+}
