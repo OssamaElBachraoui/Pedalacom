054748d056b2909497489251cc3251104aafe24c
Killmee1 <stefano.basescu@gmail.com>
2023-12-20 12:44:05 +0100

    GetOldCustomer by email

---
+++ Controllers/OldCustomersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pedalacom.Models;
+
+namespace Pedalacom.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OldCustomersController : ControllerBase
+    {
+
+        private readonly AdventureWorksLt2019Context _context;
+
+        public OldCustomersController(AdventureWorksLt2019Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{email}")]
+        public async Task<ActionResult<OldCustomer>> GetCustomer(string email)
+        {
+            var lastCustomer = await _context.OldCustomers
+                .FromSqlRaw("select * from [dbo].[View_IsOld]")
+                .Where(c => c.EmailAddress == email)
+                .OrderByDescending(c => c.CustomerId)
+                .FirstOrDefaultAsync();
+
+            if (lastCustomer == null)
+            {
+                return NotFound();
+            }
+
+            return lastCustomer;
+        }
+
+
+
+    }
+}
---
+++ Models/OldCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pedalacom.Models
+{
+    public class OldCustomer
+    {
+        [Key]
+        [Required]
+        public int CustomerId { get; set; }
+
+        public string? EmailAddress { get; set; }
+
+        public int? IsOld { get; set; }
+    }
+}
